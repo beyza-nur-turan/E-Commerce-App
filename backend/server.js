c97b07f259ec885a4d787f1a0845a78d8a5c63c8
beyza nur turan <beyzanurturan48@gmail.com>
2023-11-07 11:37:55 +0300

    added products and categories routes operations with node.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+const mainRoute = require("./routes/index.js");
 const port = 5000;
+
+dotenv.config();
 
 const connect = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://beyzanurturan48:sCy3nL3XdEFWZarf@mern-e-commerce-app.yffergs.mongodb.net/"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongoDb");
   } catch (error) {
     throw error;
   }
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello Express.js");
-});
-
-app.get("/api", (req, res) => {
-  res.send("This is API Route.");
-});
+app.use("/api", mainRoute);
 
 app.listen(port, () => {
   connect();
