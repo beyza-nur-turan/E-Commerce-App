1edbfde129473c64d2fad968d4deb0654e7da11f
beyza nur turan <beyzanurturan48@gmail.com>
2023-11-09 21:28:17 +0300

    added product create operation

---
+++ backend/models/Product.js
@@ -0,0 +1,33 @@
+const mongoose=require("mongoose");
+
+const ReviewSchema=mongoose.Schema(
+    {
+        text:{type:String,required:true},
+        rating:{type:Number,required:true},
+        user:{type:mongoose.Schema.Types.ObjectId, ref:"User",required:true}//user modeli ile ilişkilendirme kurup user id yi alıyor
+    }
+)
+
+const ProductSchema=mongoose.Schema(
+    {
+        name: {type:String, required:true},
+        img: [{type:String, required:true}],
+        reviews:[ReviewSchema],
+        description: {type:String, required:true},
+        colors:[{type:String,required:true}],
+        sizes:[{type:String,required:true}],
+        price:{
+            new:{type:Number,required:true},
+            discount:{type:Number}
+        },
+        category:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Category",
+            required:true
+        }
+
+    },
+    {timestamps:true}
+)
+const Product=mongoose.model("Product",ProductSchema)
+module.exports=Product
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Product=require("../models/Product")
 
 // Tüm ürünleri getirme (Read- All)
 router.get("/", async (req, res)=> {
     res.send("Ürünler getirildi!");
-})
+});
+//CREATE
+router.post("/", async (req, res) => {
+    try {
+      const newProduct = new Category( req.body );
+      await newProduct.save();
+  
+      res.status(201).json(newProduct); //yeni bir şey oluşturuncaki başarı kodu :201 dir
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
 
 module.exports = router;
