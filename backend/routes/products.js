dc7ef990d8be7765c3cb061964de65cbcb3e68c9
beyza nur turan <beyzanurturan48@gmail.com>
2023-12-03 19:47:49 +0300

    added option to fetch products by name

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -80,6 +80,18 @@ router.post("/", async (req, res) => {
       res.status(500).json({error:"server error"})
     }
   })
+  //ürünleri isme göre arama
+  router.get("/search/:productName",async(req,res)=>{
+    try {
+      const productName=req.params.productName;
+      const products=await Product.find({
+        name:{$regex:productName,$options:"i"},
+      });
+      res.status(200).json(products); 
+    } catch (error) {
+      console.log(error)
+    }
+  })
 
 
 module.exports = router;
