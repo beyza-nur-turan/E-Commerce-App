539ef9ceaf205c8f0c1469769d18708cf0d0f193
beyza nur turan <beyzanurturan48@gmail.com>
2024-01-19 20:17:05 +0300

    card data brought

--- frontend/E_Commerce_App/src/context/StripeProvider.jsx
+++ frontend/E_Commerce_App/src/context/StripeProvider.jsx
@@ -1,44 +1,3 @@
-<<<<<<< Updated upstream
-=======
-// import { createContext, useContext, useReducer, useState } from "react"
-
-// const initialState = {
-//     totalRevenue: null,
-//     totalProducts: null,
-//     totalCustomers: null,
-//   };
-// const StripeContext=createContext();
-// const reducer = (state, action) => {
-//     switch (action.type) {
-//       case "SET_TOTAL_REVENUE":
-//         return { ...state, totalRevenue: action.payload };
-//       case "SET_TOTAL_PRODUCTS":
-//         return { ...state, totalProducts: action.payload };
-//       case "SET_TOTAL_CUSTOMERS":
-//         return { ...state, totalCustomers: action.payload };
-//       default:
-//         return state;
-//     }
-//   };
-// function StripeProvider({children}) {
-//     const [state, dispatch] = useReducer(reducer, initialState);
-//     const [stripeData,setStripeData]=useState();
-//     const apiUrl = import.meta.env.VITE_API_BASE_URL;
-//   return <StripeContext.Provider value={{ state, dispatch }}>{children}</StripeContext.Provider>
-// }
-// const useStripeContext = () => {
-//     const context=()=>useContext(StripeContext);
-//     if (!context) {
-//       throw new Error("useAppContext must be used within an AppProvider");
-//     }
-//     return useStripeContext;
-//   };
-
-// export {StripeProvider,useStripeContext}
-
-
-
->>>>>>> Stashed changes
 import  { createContext, useReducer, useContext } from "react";
 
 const initialState = {
@@ -46,12 +5,7 @@ const initialState = {
   totalProducts: null,
   totalCustomers: null,
 };
-<<<<<<< Updated upstream
 const StripeContext = createContext();
-=======
-
-const AppContext = createContext();
->>>>>>> Stashed changes
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -61,7 +15,6 @@ const reducer = (state, action) => {
       return { ...state, totalProducts: action.payload };
     case "SET_TOTAL_CUSTOMERS":
       return { ...state, totalCustomers: action.payload };
-<<<<<<< Updated upstream
       case "SET_TOTAL_CUSTOMERS_BY_MONTH":
       return { ...state, totalCustomersByMonth: action.payload };
       case "SET_TOTAL_SALES_BY_MONTH":
@@ -88,33 +41,8 @@ export const useStripeContext = () => {
   const context = useContext(StripeContext);
   if (!context) {
     throw new Error("useStripecontext bir StripeProvider içinde kullanılmalıdır");
-=======
-    default:
-      return state;
-  }
-};
-
-const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  return (
-    <AppContext.Provider value={{ state, dispatch }}>
-      {children}
-    </AppContext.Provider>
-  );
-};
-
-const useAppContext = () => {
-  const context = useContext(AppContext);
-  if (!context) {
-    throw new Error("useAppContext must be used within an AppProvider");
->>>>>>> Stashed changes
   }
   return context;
 };
 
-<<<<<<< Updated upstream
 
-=======
-export { AppProvider, useAppContext };
->>>>>>> Stashed changes
--- frontend/E_Commerce_App/src/pages/DashboardPage.jsx
+++ frontend/E_Commerce_App/src/pages/DashboardPage.jsx
@@ -9,74 +9,21 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
-<<<<<<< Updated upstream
 import "../css/dashboardPage.css"
 import { useStripeContext } from "../context/StripeProvider";
-=======
-import { useAppContext } from "../context/StripeProvider";
->>>>>>> Stashed changes
 
 const DashboardPage = () => {
   const [paymentData, setPaymentData] = useState(null);
-  const [stripeTotalAmount, setStripeStripeTotalAmount] = useState(null);
   const apiUrl = import.meta.env.VITE_API_BASE_URL;
-<<<<<<< Updated upstream
   const { state, dispatch } = useStripeContext();
   const [customerData, setCustomerData] = useState();
   const [salesData, setSalesData] = useState([]);
-=======
-  const{state,dispatch}=useAppContext()
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const paymentResponse = await fetch(`${apiUrl}/payment`);
-  //       const paymentData = await paymentResponse.json();
-  //       setPaymentData(paymentData);
-
-  //       const stripeResponse = await fetch(`${apiUrl}/stripe`);
-  //       const stripeData = await stripeResponse.json();
-  //       setStripeData(stripeData);
-  //     } catch (error) {
-  //       console.error('Veri çekme hatası:', error.message);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
-
-  // useEffect(() => {
-  //   const fetchPaymentData = async () => {
-  //     try {
-  //       const response = await fetch(`${apiUrl}/payment`); 
-  //       const data = await response.json();
-  //       setPaymentData(data);
-  //     } catch (error) {
-  //       console.error('Error fetching payment:', error);
-  //     }
-  //   }; 
-  //   const fetchStripeData = async () => {
-  //     try {
-  //       const response = await fetch(`${apiUrl}/stripe/totalRevenue`); 
-  //       const data = await response.json();
-  //       setStripeStripeTotalAmount(data);
-  //       console.log("gelem stripe data",data)
-  //     } catch (error) {
-  //       console.error('Error fetching payment:', error);
-  //     }
-  //   }; 
-  //   fetchStripeData();
-  //   fetchPaymentData();
-  // }, []);
-
->>>>>>> Stashed changes
   useEffect(() => {
     const fetchData = async () => {
       try {
         const paymentResponse = await fetch(`${apiUrl}/stripe/totalRevenue`);
         const paymentData = await paymentResponse.json();
         dispatch({ type: "SET_TOTAL_REVENUE", payload: paymentData });
-<<<<<<< Updated upstream
         const customersResponse = await fetch(
           `${apiUrl}/stripe/totalCustomers`
         );
@@ -131,27 +78,10 @@ const DashboardPage = () => {
       }
 
       
-=======
-
-        const productsResponse = await fetch(`${apiUrl}/stripe/totalProductsSold`);
-        const productsData = await productsResponse.json();
-        dispatch({ type: "SET_TOTAL_PRODUCTS", payload: productsData });
-
-        const customersResponse = await fetch(`${apiUrl}/stripe/totalCustomers`);
-        const customersData = await customersResponse.json();
-        dispatch({ type: "SET_TOTAL_CUSTOMERS", payload: customersData });
-      } catch (error) {
-        console.error('Veri çekme hatası:', error);
-      }
->>>>>>> Stashed changes
     };
 
     fetchData();
   }, [apiUrl, dispatch]);
-<<<<<<< Updated upstream
-=======
-  console.log(paymentData)
->>>>>>> Stashed changes
   const productSalesData = [
     { name: "Ocak", satilanUrunSayisi: 10 },
     { name: "Şubat", satilanUrunSayisi: 15 },
@@ -169,15 +99,12 @@ const DashboardPage = () => {
     return monthNames[monthNumber - 1];
   };
   const currentMonth = new Date().getMonth() + 1; // Şu anki ayın sayısal değeri
-  const sortedSalesData = salesData
-    .filter(item => item.month >= currentMonth) // Şu andan sonraki ayları al
-    .sort((a, b) => a.month - b.month); // Ayları sayısal değerlerine göre sırala
+const sortedSalesData = salesData
+  .filter(item => item.month >= currentMonth) // Şu andan sonraki ayları al
+  .sort((a, b) => a.month - b.month); // Ayları sayısal değerlerine göre sırala
 const allMonthsData = customerData?.totalCustomersByMonth || [];
 
-const selectedMonth = allMonthsData.slice(-1).map(item => ({
-  monthName: getMonthName(item.month),
-  totalCustomers: item.totalCustomers,
-}));
+
 
   return (
     <div className="dashboardContainer">
@@ -193,76 +120,80 @@ const selectedMonth = allMonthsData.slice(-1).map(item => ({
         <Col span={8}>
           <Card style={{border: '3px solid #f39c12 '}}>
             <Statistic
-              title="Toplam Müşteri Sayısı(Ürün satın alan)"
+              title="Toplam Müşteri Sayısı"
               value={state.totalCustomers?.totalCustomers}
             />
           </Card>
         </Col>
         <Col span={8}>
-<<<<<<< Updated upstream
           <Card style={{border: '3px solid #2ecc71  '}}>
             <Statistic
               title="Toplam Gelir"
               value={state.totalRevenue?.totalRevenue}
               prefix="$"
             />
-=======
-          <Card>
-            <Statistic title="Toplam Gelir" value={state.totalRevenue?.totalRevenue} prefix="$" />
->>>>>>> Stashed changes
           </Card>
         </Col>
       </Row>
       <div style={{display:"grid", justifyContent:"center"}}>
       <Card style={{ marginTop: "20px", justifyContent: "center", border: '2px solid #f0f0f0', boxShadow: '0 2px 4px black' }}>
-        <h2>Son Aydaki Ürün Satış Artışı</h2>
-        <LineChart
-          width={750}
-          height={400}
-          data={salesData.map(item => ({
-            month: getMonthName(item.month),
-            satilanUrunSayisi: item.satilanUrunSayisi,
-          }))}
-          margin={{ top: 5, right: 30, bottom: 5 }}
-        >
-          <XAxis dataKey="month" reversed="true" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="satilanUrunSayisi"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-        </LineChart>
-      </Card>
+  <h2>Son Aydaki Ürün Satış Artışı</h2>
+  <LineChart
+    width={750}
+    height={400}
+    data={salesData.map(item => ({
+      month: getMonthName(item.month),
+      satilanUrunSayisi: item.satilanUrunSayisi,
+    }))}
+    margin={{ top: 5, right: 30, bottom: 5 }}
+  >
+    <XAxis dataKey="month" reversed={true} />
+    <YAxis />
+    <CartesianGrid strokeDasharray="3 3" />
+    <Tooltip />
+    <Legend />
+    <Line
+      type="monotone"
+      dataKey="satilanUrunSayisi"
+      stroke="#8884d8"
+      activeDot={{ r: 8 }}
+    />
+  </LineChart>
+</Card>
 
-      <Card style={{ marginTop: "20px", display:"flex", justifyContent:"center",border: '2px solid #f0f0f0',boxShadow: '0 2px 4px black' }}>
-        <h2>Son Aydaki Müşteri Artışı</h2>
-        <LineChart 
-          width={450}
-          height={300}
-          data={allMonthsData.map(item => ({
-            monthName: getMonthName(item.month),
-            totalCustomers: item.totalCustomers,
-          }))}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+<Card
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            justifyContent: "center",
+            border: "2px solid #f0f0f0",
+            boxShadow: "0 2px 4px black",
+          }}
         >
-          <XAxis dataKey="monthName" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-             dataKey="totalCustomers"
-            stroke="#82ca9d"
-            activeDot={{ r: 8 }}
-          />
-        </LineChart>
-      </Card>
+          <h2>Son Aydaki Müşteri Artışı</h2>
+          <LineChart
+            width={450}
+            height={300}
+            data={allMonthsData.map((item) => ({
+              monthName: getMonthName(item.month),
+              totalCustomers: item.totalCustomers,
+            }))}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <XAxis dataKey="monthName" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="totalCustomers"
+              stroke="#82ca9d"
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </Card>
+
       </div>
     </div>
   );
